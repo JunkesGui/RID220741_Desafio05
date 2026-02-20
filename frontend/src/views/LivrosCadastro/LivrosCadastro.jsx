b6c8fc08b8c4aca61e8d98df308e079d95a6fa43
junkesgui <guilherme.junkes1@gmail.com>
2026-02-20 07:55:10 -0300

    fix: logica de integracao no frontend

--- frontend/src/views/LivrosCadastro/LivrosCadastro.jsx
+++ frontend/src/views/LivrosCadastro/LivrosCadastro.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import Header from '../../components/Header/Header'
 import "./index.scss"
 import SubmenuLivros from '../../components/SubmenuLivros/SubmenuLivros'
@@ -19,11 +19,11 @@ const LivrosCadastro = () => {
       if(livro.id!=undefined && livro.id!='' && livro.titulo!=undefined && livro.titulo!='' && livro.num_paginas!=undefined && livro.num_paginas!='' && livro.isbn !=undefined && livro.isbn !='' && livro.editora !=undefined && livro.editora !=''){
       await LivrosService.createLivro(body)
       .then((response)=>{
-        alert(response.data)
-        document.getElementById('formulario').reset
+        alert(`Livro ${response.data.titulo} cadastrado com sucesso!`)
+        document.getElementById('formulario').reset()
       })
       .catch(({response:{data,status}})=>{
-        alert(`${status} - ${data}`)      
+        alert(`${status} - ${data.message}`)    
       });
     }
 
@@ -58,8 +58,10 @@ const LivrosCadastro = () => {
             <input type="text" id='editora' required onChange={(event)=>{ setLivro({...livro, editora: event.target.value})}}></input>
           </div> 
           <div className='form-group'>
-            <button onClick={()=>{
+            <button onClick={(e)=>{
+              e.preventDefault()
               createLivro()
+              document.getElementById('formulario').reset()
             }}>Cadastrar Livro</button>  
           </div>         
           </form>
