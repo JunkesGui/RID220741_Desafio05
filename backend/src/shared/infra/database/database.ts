95e6ee91e64d6fdb2ffc3a68ac47658810a2236b
junkesgui <guilherme.junkes1@gmail.com>
2026-03-20 10:43:29 -0300

    refactor: remove a db docker e move para mariadb remoto

--- backend/src/shared/infra/database/database.ts
+++ backend/src/shared/infra/database/database.ts
@@ -1,16 +1,16 @@
-require("dotenv/config");
-require("reflect-metadata");
+import "dotenv/config";
+import "reflect-metadata";
 import { DataSource } from "typeorm";
 
-const port = process.env.POSTGRES_PORT as number | undefined;
+const port = process.env.MARIA_PORT as number | undefined;
 
 export const AppDataSource = new DataSource({
-  type: "postgres",
-  host: process.env.POSTGRES_HOST,
+  type: "mariadb",
+  host: process.env.MARIA_HOST,
   port: port,
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
+  username: process.env.MARIA_USER,
+  password: process.env.MARIA_PASSWORD,
+  database: process.env.MARIA_DB,
   entities: ["./src/modules/**/entities/*.ts"],
   migrations: ["./src/shared/infra/database/migrations/*.ts"],
 });
